08592ac1180a74fda68ee2a1dd2dc49c8579522a
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-11-02 23:56:33 +0100

    Initial commit

---
+++ src/cacttus/education/datatypes_and_variables/UshtrimeMeInteger.java
@@ -0,0 +1,12 @@
+package cacttus.education.datatypes_and_variables;
+
+public class UshtrimeMeInteger {
+
+    public static void main(String[] args) {
+        //deklarimi i variables
+        byte mosha=125;
+        System.out.println(mosha);
+        mosha = 5;
+        System.out.println(mosha);
+    }
+}
