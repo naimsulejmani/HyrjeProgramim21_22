7dd0bdfa719256ae4bd3a661abd9695dff6f6d78
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-11-03 00:00:02 +0100

    CV, format gjeometrike

---
+++ src/cacttus/education/terminal_system_out/FormatVizualizuese.java
@@ -0,0 +1,44 @@
+package cacttus.education.terminal_system_out;
+
+public class FormatVizualizuese {
+    public static void main(String[] args) {
+        System.out.println("CV");
+        System.out.println("-------------------------------");
+        System.out.println("Emri:           Naim Sulejmani");
+        System.out.println("Mbiemri:        Sulejmani");
+        System.out.println("Titulli:        Professor");
+        System.out.println("Qyteti:         Shkup");
+        System.out.println("Adresa:         Nuk ju tregoj!");
+        System.out.println("Gjinia:         M");
+        System.out.println("-------------------------------");
+
+        System.out.println("Trekendeshi");
+        System.out.println("---------------------------");
+        System.out.println("   .   ");
+        System.out.println("  .  .  ");
+        System.out.println(" .    . ");
+        System.out.println("........");
+
+        System.out.println("Katrori");
+        System.out.println("---------------------------");
+        System.out.println("------");
+        System.out.println("|    |");
+        System.out.println("------");
+        System.out.println("\u263A");
+
+
+        System.out.println("HANGMAN GAME");
+        System.out.println("             ___             ");
+        System.out.println("           | .  .|          ");
+        System.out.println("            \\ _ /");
+        System.out.println("             '''");
+
+        System.out.println("              |");
+        System.out.println("             /|\\");
+        System.out.println("            / | \\");
+        System.out.println("           /  |  \\");
+        System.out.println("             / \\");
+        System.out.println("            /   \\");
+        System.out.println("           /     \\");
+    }
+}
